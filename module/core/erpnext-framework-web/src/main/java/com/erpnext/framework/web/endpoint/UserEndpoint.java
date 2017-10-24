35deeaa98877141fca612360dcf2a32d89375b2a
heshm <hsmhch@163.com>
2017-10-25 07:55:58 +0800

    343

--- module/core/erpnext-framework-web/src/main/java/com/erpnext/framework/web/endpoint/UserEndpoint.java
+++ module/core/erpnext-framework-web/src/main/java/com/erpnext/framework/web/endpoint/UserEndpoint.java
@@ -3,11 +3,13 @@ package com.erpnext.framework.web.endpoint;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.erpnext.framework.domain.AdminUser;
 import com.erpnext.framework.service.SiteSecurityService;
+import com.erpnext.framework.web.util.AuthenticationUtils;
 
 @RestController
 public class UserEndpoint extends BaseEndpoint{
@@ -19,6 +21,12 @@ public class UserEndpoint extends BaseEndpoint{
 	public List<AdminUser> getAllUser() {
 		logger.debug("Rest controller {} is called.","UserEndpoint");
 		return siteSecurityService.getAllUser();
+	}
+	
+	@GetMapping("/getLoginUserInfo")
+	public AdminUser getLoginUserInfo() {
+		UserDetails userDetails = AuthenticationUtils.getPrincipal();
+		return siteSecurityService.readAdminUserByLoginName(userDetails.getUsername());
 	}
 
 }
