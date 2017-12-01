176e05180e31072fed9574279ccc23d15b8acfbc
heshm <hsmhch@163.com>
2017-12-01 13:49:35 +0800

    232

--- module/app/erpnext-common/src/main/java/com/erpnext/common/param/domain/DictType.java
+++ module/app/erpnext-common/src/main/java/com/erpnext/common/param/domain/DictType.java
@@ -4,6 +4,8 @@ public class DictType {
     private String id;
 
     private String name;
+    
+    private Integer valueLength;
 
     public String getId() {
         return id;
@@ -20,4 +22,14 @@ public class DictType {
     public void setName(String name) {
         this.name = name;
     }
+
+	public Integer getValueLength() {
+		return valueLength;
+	}
+
+	public void setValueLength(Integer valueLength) {
+		this.valueLength = valueLength;
+	}
+    
+    
 }
