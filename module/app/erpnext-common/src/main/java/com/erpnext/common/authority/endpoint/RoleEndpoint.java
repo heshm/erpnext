d7d4464b75c1694f5e9c4fa052da644e29ea32b6
heshm <hsmhch@163.com>
2017-12-10 13:47:29 +0800

    34

--- module/app/erpnext-common/src/main/java/com/erpnext/common/authority/endpoint/RoleEndpoint.java
+++ module/app/erpnext-common/src/main/java/com/erpnext/common/authority/endpoint/RoleEndpoint.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.erpnext.common.authority.dto.UserRoleDTO;
 import com.erpnext.common.authority.service.RoleService;
 import com.erpnext.framework.domain.AdminRole;
 import com.erpnext.framework.web.endpoint.BaseEndpoint;
@@ -26,6 +28,11 @@ public class RoleEndpoint extends BaseEndpoint {
 		return roleService.getAllRole();
 	}
 	
+	@GetMapping("/userRole/{userId}")
+	public UserRoleDTO userRole(@PathVariable("userId")String userId) {
+		return roleService.getUserRole(userId);
+	}
+	
 	@PostMapping("/create")
 	public String create(@RequestBody AdminRole role) {
 		roleService.saveRole(role);
@@ -36,6 +43,12 @@ public class RoleEndpoint extends BaseEndpoint {
 	public String delete(@PathVariable("id") String id) {
 		roleService.deleteRole(id);
 		return DELETED;
+	}
+	
+	@PutMapping("/update")
+	public String update(@RequestBody UserRoleDTO userRoleDTO) {
+		roleService.updateUserRole(userRoleDTO);
+		return UPDATED;
 	}
 
 }
