8555c5486d73c6ab80e48af2de37c9fa4ed536b0
heshm <hsmhch@163.com>
2018-01-12 17:04:07 +0800

    finish authority

--- module/app/erpnext-common/src/main/java/com/erpnext/common/authority/endpoint/PermissionEndpoint.java
+++ module/app/erpnext-common/src/main/java/com/erpnext/common/authority/endpoint/PermissionEndpoint.java
@@ -4,12 +4,15 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.erpnext.common.authority.dto.PermissionDTO;
+import com.erpnext.common.authority.dto.RolePermissionDTO;
+import com.erpnext.common.authority.dto.UserPermissionDTO;
 import com.erpnext.common.authority.service.PermissionService;
 import com.erpnext.framework.domain.Permission;
 import com.erpnext.framework.web.endpoint.BaseEndpoint;
@@ -21,9 +24,36 @@ public class PermissionEndpoint extends BaseEndpoint {
 	@Autowired
 	private PermissionService permissionService;
 	
-	@GetMapping("/list")
-	public List<Permission> list(){
+	@GetMapping("/tree_list")
+	public List<Permission> treeList(){
 		return permissionService.getNestedPermission();
+	}
+	
+	@GetMapping("/list")
+	public List<PermissionDTO> list(){
+		return permissionService.getAllPermission();
+	}
+	
+	@GetMapping("/role_perm/{roleId}")
+	public RolePermissionDTO rolePerm(@PathVariable("roleId")String roleId){
+		return permissionService.getPermissionByRole(roleId);
+	}
+	
+	@PostMapping("/role_perm")
+	public RolePermissionDTO rolePerm(@RequestBody RolePermissionDTO rolePerm){
+		permissionService.updateRolePermission(rolePerm);
+		return rolePerm;
+	}
+	
+	@GetMapping("/user_perm/{userId}")
+	public UserPermissionDTO userPerm(@PathVariable("userId")String userId){
+		return permissionService.getSpecialPermissionByUser(userId);
+	}
+	
+	@PostMapping("/user_perm")
+	public String userPerm(@RequestBody UserPermissionDTO userPerm){
+		permissionService.updateUserPermission(userPerm);
+		return UPDATED;
 	}
 	
 	@PostMapping("/create")
